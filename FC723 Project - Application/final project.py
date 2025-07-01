c0db7bae074c99396118f8cc4b775b75e238f7cd
Lynn <jning000000524@gmail.com>
2025-07-01 19:27:18 +0100

    display seat layout

--- FC723 Project - Application/final project.py
+++ FC723 Project - Application/final project.py
@@ -6,4 +6,23 @@ Created on Thu Jun 26 10:48:12 2025
 @author: jninggggggh
 """
 
-hi
+# F = Free 
+# R = Reserved 
+# X = Aisle 
+# S = Storage 
+
+seat_matrix = [
+    ['F', 'F', 'X', 'F', 'F'],
+    ['F', 'S', 'X', 'S', 'F'],
+    ['F', 'F', 'X', 'F', 'F'],
+]
+
+# show the floor plan of Burak757
+def display_seat_layout():
+    print("\nSeat Layout:")
+    for i, row in enumerate(seat_matrix):
+        row_label = chr(65 + i)  #0 → A，1 → B, 2 → C
+        print(row_label + ' ' + ' '.join(row))
+    print()
+
+print(display_seat_layout())
