1c3703911a158a899ee3a40ad9ba8da8f4dd0bf8
Lynn <jning000000524@gmail.com>
2025-07-01 20:01:51 +0100

    SQLite

--- FC723 Project - Application/final project.py
+++ FC723 Project - Application/final project.py
@@ -5,6 +5,31 @@ Created on Thu Jun 26 10:48:12 2025
 
 @author: jninggggggh
 """
+import sqlite3
+import random
+import string
+
+# connect SQLite 
+conn = sqlite3.connect("booking.db")
+cursor = conn.cursor()
+
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS bookings (
+    reference TEXT PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    passport TEXT,
+    seat_id TEXT
+)
+''')
+conn.commit()
+
+
+
+
+
+
 
 # F = Free 
 # R = Reserved 
@@ -80,64 +105,88 @@ def check_availability():
     elif status == 'S':
         print(f"❌ Seat {seat_id} is storage. Not a bookable seat.\n")
         
+
+# generate the unique 8 words reference
+def generate_booking_reference():
+    while True:
+        ref = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+        cursor.execute("SELECT * FROM bookings WHERE reference = ?", (ref,))
+        if cursor.fetchone() is None:
+            return ref
+
+
 #book a seat
 def book_seat():
     seat_id = input("Enter seat ID to book (e.g., A0): ").upper()
-    
+
     if len(seat_id) != 2 or not seat_id[0].isalpha() or not seat_id[1].isdigit():
         print("Invalid seat ID format.\n")
         return
-    
+
     row_char = seat_id[0]
     col_index = int(seat_id[1])
     row_index = ord(row_char) - ord('A')
 
-    #check if the seat is in the right area
     if row_index < 0 or row_index >= len(seat_matrix) or col_index < 0 or col_index >= len(seat_matrix[0]):
         print("Seat ID out of range.\n")
         return
 
     current_status = seat_matrix[row_index][col_index]
-    
-    if current_status == 'F':
-        seat_matrix[row_index][col_index] = 'R'  #change it to already book
-        print(f"✅ Seat {seat_id} booked successfully!\n")
-    elif current_status == 'R':
-        print(f"❌ Seat {seat_id} is already reserved.\n")
-    elif current_status == 'X':
-        print(f"❌ Seat {seat_id} is an aisle. You cannot book it.\n")
-    elif current_status == 'S':
-        print(f"❌ Seat {seat_id} is storage. You cannot book it.\n")
+
+    if current_status != 'F':
+        print(f"❌ Seat {seat_id} is not available for booking.\n")
+        return
+
+    #enter the info 
+    first = input("Enter first name: ")
+    last = input("Enter last name: ")
+    passport = input("Enter passport number: ")
+
+    # generate unique booking reference
+    ref = generate_booking_reference()
+
+    #update floor plan
+    seat_matrix[row_index][col_index] = 'R'
+
+    #enter SQLite
+    cursor.execute('''
+        INSERT INTO bookings (reference, first_name, last_name, passport, seat_id)
+        VALUES (?, ?, ?, ?, ?)
+    ''', (ref, first, last, passport, seat_id))
+    conn.commit()
+
+    print(f"✅ Seat {seat_id} booked successfully! Booking reference: {ref}\n")
+
 
 
 #free a seat
 def free_seat():
     seat_id = input("Enter seat ID to free (e.g., A0): ").upper()
-    
+
     if len(seat_id) != 2 or not seat_id[0].isalpha() or not seat_id[1].isdigit():
         print("Invalid seat ID format.\n")
         return
-    
+
     row_char = seat_id[0]
     col_index = int(seat_id[1])
     row_index = ord(row_char) - ord('A')
 
-    #check seat area
     if row_index < 0 or row_index >= len(seat_matrix) or col_index < 0 or col_index >= len(seat_matrix[0]):
         print("Seat ID out of range.\n")
         return
 
-    status = seat_matrix[row_index][col_index]
-    
-    if status == 'R':
-        seat_matrix[row_index][col_index] = 'F'  #change back to aviliably
-        print(f"✅ Seat {seat_id} is now free.\n")
-    elif status == 'F':
-        print(f"ℹ️ Seat {seat_id} is already free.\n")
-    elif status == 'X':
-        print(f"❌ Seat {seat_id} is an aisle. It was never reservable.\n")
-    elif status == 'S':
-        print(f"❌ Seat {seat_id} is storage. It was never reservable.\n")
+    if seat_matrix[row_index][col_index] != 'R':
+        print(f"❌ Seat {seat_id} is not reserved.\n")
+        return
+
+    #delete the infro from store
+    cursor.execute("DELETE FROM bookings WHERE seat_id = ?", (seat_id,))
+    conn.commit()
+
+    #update floor plan
+    seat_matrix[row_index][col_index] = 'F'
+    print(f"✅ Seat {seat_id} is now free.\n")
+
 
 
 
